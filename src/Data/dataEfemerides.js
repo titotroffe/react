c79cb5dd6ab0cf8fea8d9eb8eca5010d8b3e2414
Tito <titotroffe@hotmail.com>
2025-07-17 21:35:32 -0300

    Arregladas las rutas

--- src/Data/dataEfemerides.js
+++ src/Data/dataEfemerides.js
@@ -1,22 +1,22 @@
 export default[
     {
         titulo: "Agasajo a los campeones",
-        imagen: "src/img/Efemerides/Foto1.jpg",
+        imagen: "/img/Efemerides/Foto1.jpg",
         descripcion: "A las 21hs se realizaba un gran asado criollo para agasajar a los campeones del año 1958"
     },
     {
         titulo: "Campeon en veteranos",
-        imagen: "src/img/Efemerides/Foto2.png",
+        imagen: "/img/Efemerides/Foto2.png",
         descripcion: "Victoria 1 a 0 en la final frente a Cabotaje"
     },
     {
         titulo: "Nannini defiende la victoria",
-        imagen: "src/img/Efemerides/Foto3.png",
+        imagen: "/img/Efemerides/Foto3.png",
         descripcion: "Parana buscaba el gol pero chocaba contra los puños de Nannini"
     },
     {
         titulo: "Viaje a Campana",
-        imagen: "src/img/Efemerides/Foto4.png",
+        imagen: "/img/Efemerides/Foto4.png",
         descripcion: "Centenares de nicoleños reservaban sus lugares para el viaje a Campana"
     },
 ]
--- src/Data/proxPartidos.js
+++ src/Data/proxPartidos.js
@@ -1,33 +1,33 @@
 export default [
     {
         fecha: "Fecha 5",
-        escudoL: "src/img/Escudos/ArgOeste.png",
+        escudoL: "/img/Escudos/ArgOeste.png",
         nombreL: "Argentino Oeste",
-        escudoV: "src/img/Escudos/social.png",
+        escudoV: "/img/Escudos/social.png",
         nombreV: "Social",
         detalle: "Domingo 27 de Julio 15:00hs" 
     },
     {
         fecha: "Fecha 6",
-        escudoL: "src/img/Escudos/Defe.png",
+        escudoL: "/img/Escudos/Defe.png",
         nombreL: "Defensores de Belgrano",
-        escudoV: "src//img/Escudos/ArgOeste.png",
+        escudoV: "/img/Escudos/ArgOeste.png",
         nombreV: "Argentino Oeste",
         detalle: "Domingo 3 de Agosto 15:00hs" 
     },
     {
         fecha: "Fecha 7",
-        escudoL: "src/img/Escudos/ArgOeste.png",
+        escudoL: "/img/Escudos/ArgOeste.png",
         nombreL: "Argentino Oeste",
-        escudoV: "src/img/Escudos/parana.png",
+        escudoV: "/img/Escudos/parana.png",
         nombreV: "Parana",
         detalle: "Domingo 10 de Agosto 15:00hs" 
     },
     {
         fecha: "Fecha 8",
-        escudoL: "src/img/Escudos/12deoct.png",
+        escudoL: "/img/Escudos/12deoct.png",
         nombreL: "12 de Octubre",
-        escudoV: "src/img/Escudos/ArgOeste.png",
+        escudoV: "/img/Escudos/ArgOeste.png",
         nombreV: "Argentino Oeste",
         detalle: "Domingo 17 de Agosto 15:00hs" 
     }
